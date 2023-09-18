26e8f2fc5b90e0d72bbc66724fcb3e256484c001
ashu sagar <itsashusagar@gmail.com>
2023-09-18 08:22:51 +0530

    commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Helloooo");
+});
+app.listen(3000, () => {
+  console.log("Server is Started.");
+});
